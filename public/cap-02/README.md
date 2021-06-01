04f6902eb4084f6540a6d801cbed999723399c84
Anderson Silva <anderson.silva@matera.com>
2021-06-01 20:30:32 -0300

    first commit

---
+++ public/cap-02/README.md
@@ -0,0 +1,3 @@
+Esse capitulo utiliza o pattern Data Mapper, tentando copiar a estrutura utilizada pelo Hibernate, porém utilizando o indexedDB do javascript.
+
+Objeto é sinonimo de store no indexedDB
---
+++ public/cap-02/app/infra/session-factory.js
@@ -0,0 +1,75 @@
+export class SessionFactory {
+  constructor(config) {
+    this.dbName = config.dbName;
+    this.dbVersion = config.dbVersion;
+    this.stores = new Map();
+    config.mappers.forEach((mapper) => {
+      this.stores.set(mapper.clazz.name, mapper.converter);
+    });
+  }
+
+  async openSession() {
+    const connection = await createConnection(
+      this.dbName,
+      this.dbVersion,
+      this.stores
+    );
+
+    return new Session(connection, this.stores);
+  }
+}
+
+class Session {
+  constructor(connection, stores) {
+    this.connection = connection;
+    this.stores = stores;
+  }
+
+  save(object) {
+    return new Promise((resolve, reject) => {
+      const storeName = object.constructor.name;
+      const request = this.connection
+        .transaction([storeName], "readwrite")
+        .objectStore(storeName)
+        .add(object);
+
+      request.onsuccess = (e) => resolve();
+
+      request.onerror = (e) => {
+        console.log(e.target.error);
+        reject(`Não foi possível persistir o objeto na store ${storeName}`);
+      };
+    });
+  }
+}
+
+function createConnection(dbName, dbVersion, stores) {
+  return new Promise((resolve, reject) => {
+    const request = window.indexedDB.open(dbName, dbVersion);
+
+    request.onupgradeneeded = (e) => {
+      const transactionalConnection = e.target.result;
+      for (let [key] of stores) {
+        const store = key;
+
+        if (transactionalConnection.objectStoreNames.contains(store)) {
+          transactionalConnection.deleteObjectStore(store);
+        }
+
+        transactionalConnection.createObjectStore(store, {
+          autoIncrement: true,
+        });
+      }
+    };
+
+    request.onsuccess = (e) => {
+      const connection = e.target.result;
+      resolve(connection);
+    };
+
+    request.onerror = (e) => {
+      console.log(e.target.error);
+      reject(`Não foi possível obter a conexão com o banco ${dbName}`);
+    };
+  });
+}
